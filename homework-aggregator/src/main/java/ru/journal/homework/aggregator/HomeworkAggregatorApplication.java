6df86e48ef4866b881782fff2a29a705ee9a2674
no.invited <bekas2003@mail.ru>
2024-11-12 12:28:32 +0300

    Создал проект диплома, добавил начальные зависимости, создал репозиторий

---
+++ homework-aggregator/src/main/java/ru/journal/homework/aggregator/HomeworkAggregatorApplication.java
@@ -0,0 +1,13 @@
+package ru.journal.homework.aggregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkAggregatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeworkAggregatorApplication.class, args);
+	}
+
+}
